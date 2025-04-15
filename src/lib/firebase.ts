c2aeded6dcc310e487c115aafe75fadbfe749f06
Lucas Barros <lucasweacked21@gmail.com>
2025-04-15 08:24:47 -0300

    feat: adicao do conteudo da aula de ontem.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,23 @@
-import { initializeApp } from "firebase-admin";
-import { cert, getApp, getApps } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 // import { getStorage } from "firebase-admin/storage";
 import "server-only";
 
-const decodeKey = Buffer.from(
+const decodedKey = Buffer.from(
   process.env.FIREBASE_PRIVATE_KEY_BASE64!,
   "base64"
 ).toString("utf-8");
 
-const firebaseCert = cert({
+export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: decodedKey,
 });
 
 if (!getApps().length) {
   initializeApp({
     credential: firebaseCert,
-    //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
